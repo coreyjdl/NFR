f44240db085af10ec18ca5d16e305fa0f0d91376
coreyjdl <coreyjdl@gmail.com>
2020-12-30 15:18:56 -0600

    wathed more, added percentage seen.

--- NFRFilter.py
+++ NFRFilter.py
@@ -2,6 +2,9 @@
 
 from random import choice
 
+def percent(total, seen):
+	return 100 - int((100 / len(total)) * len(seen))
+
 with open("NFR.txt") as file:
 	films = [line.split("\t") for line in file]
 
@@ -10,6 +13,9 @@ unseen_films = [film for film in films if not film[0]]
 print(	"",
 		f"Total Films: {len(films)}",
 		f"Remaining Films: {len(unseen_films)}",
+		f"Percent of films seen: {percent(films, unseen_films)}%",
 		"Here is a random suggestion from the remaining films.",
 		"{1} - {2}".format(*choice(unseen_films)),
 		sep="\n")
+
+
